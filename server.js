32888231245a46b68e3cf9e9451b804a779d68d9
Maribel <lwang79@zoho.com>
2022-04-30 11:07:30 -0700

    vote added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./routes/api');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -12,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
