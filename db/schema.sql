037815f7435c103b407f0ef68f58e812303bfc25
Maribel <lwang79@zoho.com>
2022-04-30 10:40:53 -0700

    cleaned up the code a bit

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,3 @@
-DROP DATABASE IF EXISTS just_tech_news_db;
+DROP DATABASE IF EXISTS  just_tech_news_db;
 
-CREATE DATABASE just_tech_news_db;
-
-USE just_tech_news_db;
+CREATE DATABASE  just_tech_news_db;
