3ec722718852d8b8300eb1270a5f7d721f62e002
Maribel <lwang79@zoho.com>
2022-05-05 18:53:53 -0700

    added a login form handler

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,4 @@
+// sign up form handler
 async function signupFormHandler(event) {
   event.preventDefault();
 
@@ -25,5 +26,32 @@ async function signupFormHandler(event) {
     }
   }
 }
+
+// login form handler
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
   
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
